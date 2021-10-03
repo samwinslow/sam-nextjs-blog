e990afdeaa609a7d00a0509fa8d227030474fdad
samwinslow <sammywinslow@gmail.com>
2021-10-03 16:14:06 -0400

    nprogress loading animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/global.css'
-import '../components/Layout.css'
+import '../styles/nprogress.css'
 import '../styles/utils.css'
+import '../components/Layout.css'
 import { AppProps } from 'next/app'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
 
 const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
 
