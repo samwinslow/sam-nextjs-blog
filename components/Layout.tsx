df8b9ee30f8037ff123add2391838e6bd86cd21d
Sam Winslow <4645779+samwinslow@users.noreply.github.com>
2024-11-19 20:13:16 -0500

    fix image issue and add ifr post

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/legacy/image'
+import Image from 'next/image'
 import ColoredLink from './ColoredLink'
 import { Heading } from './Heading'
 import { copyrightHolder, siteTitle } from '../lib/constants'
