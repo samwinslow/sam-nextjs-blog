f2af40a734f3fd184f0c46334997b9955cc933df
Sam Winslow <4645779+samwinslow@users.noreply.github.com>
2025-06-03 09:41:16 -0400

    replace default favicon (long overdue)

--- components/Head.tsx
+++ components/Head.tsx
@@ -15,8 +15,12 @@ const Head = ({
   children = null,
 }: HeadProps) => (
   <NextHead>
-    <link rel="icon" href="/favicon.ico" />
     <title>{title}</title>
+    <link rel="icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
     <meta name="description" content={description || headline} />
     <meta property="og:image" content={ogImage || ogImageURI} />
     <meta name="og:title" content={title} />
